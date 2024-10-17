4d2acbe77502f84bc0bfd65210297330cb10081a
Birdbh <birdlkjlkj123@gmail.com>
2024-10-17 14:03:14 -0700

    update ability to handle changes to the values being watched

--- src/coms/OpcuaClient.py
+++ src/coms/OpcuaClient.py
@@ -1,21 +1,34 @@
 from opcua import Client
 
+URL = "opc.tcp://127.0.0.1:12345"
+NODE_ADDRESS_LIST = ['ns=2;s="TS1_Temperature"']
+
 class SubHandler(object):
     def datachange_notification(self, node, val, data):
         print("New data change event at NodeID", node, ". New value = ", val)
 
-if __name__ == "__main__":
-    
-    client = Client("opc.tcp://127.0.0.1:12345")
+class Client():
 
-    try:
-        client.connect()
-        temp = client.get_node('ns=2;s="TS1_Temperature"')
+    def __init__(self):
+        self.client = Client(URL)
+
+        try:
+            self.client.connect()
+            self.get_nodes()
+            while True:
+                pass
+
+        finally:
+            self.client.disconnect()
+
+    def get_nodes(self):
+        self.node_array = []
+        for node in NODE_ADDRESS_LIST:
+            self.node_array.append(self.client.get_node(node))
+            self.subscribe_node(node)
+
+    def subscribe_node(self, node):
         handler = SubHandler()
-        sub = client.create_subscription(500, handler)
-        handle = sub.subscribe_data_change(temp)
-        while True:
-            pass
+        sub = self.client.create_subscription(500, handler)
+        handle = sub.subscribe_data_change(node)
 
-    finally:
-        client.disconnect()
