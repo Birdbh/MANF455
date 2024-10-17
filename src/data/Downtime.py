90af66a2365311d55485f5a1974952a4f4e1a78a
Birdbh <birdlkjlkj123@gmail.com>
2024-10-17 15:54:00 -0700

    downtime events now store the total time, starttime, endtime so that we can calculate that later

--- src/data/Downtime.py
+++ src/data/Downtime.py
@@ -1,7 +1,8 @@
-from sqlalchemy import create_engine, Column, Integer, String, Enum
+from sqlalchemy import create_engine, Column, Integer, String, Enum, DateTime, Interval
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from data import DatabaseConnector
+import datetime
 
 Base = declarative_base()
 
@@ -11,6 +12,9 @@ class Downtime(Base):
     downtimeId = Column(Integer, primary_key=True, autoincrement=True)
     employeeId = Column(Integer, nullable=False)
     downtimeReason = Column(Enum("Machine Fault", "Product Malfunction", "Labour Incident", name="downtime_reason_types"), nullable=False)
+    downtimeStart = Column(DateTime, nullable=False)
+    downtimeEnd = Column(DateTime, nullable=True)
+    downtimeDelta = Column(Interval, nullable=True)
     status = Column(Enum("Pending", "Resolved", name="status_types"), nullable=False)
 
 class DowntimeTable:
@@ -24,16 +28,21 @@ class DowntimeTable:
 
     def add_downtime(self, employeeId: int, downtimeReason: str):
         session = self.Session()
-        new_downtime = Downtime(employeeId=employeeId, downtimeReason=downtimeReason, status='Pending')
+        current_time = datetime.datetime.now()
+        new_downtime = Downtime(employeeId=employeeId, downtimeReason=downtimeReason, status='Pending', downtimeStart=current_time)
         session.add(new_downtime)
         session.commit()
         session.close()
 
     def get_last_row(self):
         session = self.Session()
-        last_downtime = session.query(Downtime).order_by(Downtime.downtimeId.desc()).first()
-        session.close()
-        return last_downtime
+        try:
+            last_downtime = session.query(Downtime).order_by(Downtime.downtimeId.desc()).first()
+            session.close()
+            return last_downtime
+        except:
+            session.close()
+            return None
 
     def is_currently_downtime(self):
         return self.get_last_row_status() == 'Pending'
@@ -50,6 +59,8 @@ class DowntimeTable:
         session = self.Session()
         last_downtime = session.query(Downtime).order_by(Downtime.downtimeId.desc()).first()
         if last_downtime:
+            last_downtime.downtimeEnd = datetime.datetime.now()
+            last_downtime.downtimeDelta = last_downtime.downtimeEnd - last_downtime.downtimeStart
             last_downtime.status = 'Resolved'
             session.commit()
         session.close()
