94ef11add1f340ba23280b72e2d25bd10306efda
Birdbh <birdlkjlkj123@gmail.com>
2024-10-21 16:25:25 -0700

    we need a queue method to handle orders being sent at the same time

---
+++ src/coms/Queue.py
@@ -0,0 +1,15 @@
+#TODO: Create a class that will handle the queue of the orders being sent to the drilling machine.
+# This class should have the following methods:
+# - add_order: This method should add a new order to the queue.
+# - get_next_order: This method should return the next order in the queue.
+# - remove_order: This method should remove the next order in the queue.
+# - get_queue: This method should return the entire queue.
+# - clear_queue: This method should remove all the orders in the queue.
+# The class should have the following attributes:
+# - queue: This attribute should be a list that will store the orders in the queue.
+# The class should have the following constructor:
+# - __init__: This method should initialize the queue attribute as an empty list.
+# The class should have the following properties:
+# - next_order: This property should return the next order in the queue.
+# - queue_length: This property should return the length of the queue.
+# The class should have the following methods:
