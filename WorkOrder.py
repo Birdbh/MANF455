7835e9560dd422a4c0ca1f9ab56a71997cffdd02
Birdbh <birdlkjlkj123@gmail.com>
2024-10-28 17:05:21 -0700

    Deleted unneeded files

--- WorkOrder.py
+++
@@ -1,6 +0,0 @@
-class WorkOrder:
-    def __init__(self, order_id, customer_id, drilling_location, start_time):
-        self.order_id = order_id
-        self.customer_id = customer_id
-        self.drilling_location = drilling_location
-        self.start_time = start_time
